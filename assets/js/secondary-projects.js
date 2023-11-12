c1de4433a86b7f5d9dcf528e0a30509d284749af
Lucas Numeriano <116529579+Elasukou@users.noreply.github.com>
2023-11-12 00:02:01 -0300

    update desing

---
+++ assets/js/secondary-projects.js
@@ -0,0 +1,30 @@
+function addProject(linkName, name, iconTechnology, description, namePhoto,) {
+    return `
+        <a href="https://github.com/Elasukou/${linkName}" target="blank">
+            <div class="secondarys-projects">
+                    <div class="name-and-description-proj">
+                        <h3 class="title-project">${name}.</h3>
+                        <div class="technologyProject">
+                            ${iconTechnology}
+                        </div>
+                    </div>
+                    <img src="assets/images/${namePhoto}.svg" alt="${description}" class="project-preview">
+                
+            </div>
+        </a>
+    `
+}
+
+function addTechnologies(technology) {
+    return `
+        <img src="assets/images/${technology}.svg" class="iconTechnologyProject">
+    `
+}
+//<p class="project-description">${description}</p>
+
+document.querySelector(".small-base-2").innerHTML =
+    addProject("alura-spa", "Alura SPA", addTechnologies("html5") + addTechnologies("css3") + addTechnologies("sass"), "Landing Page de um SPA desenvolvido com SASS.", "alura-spa-preview") +
+
+    addProject("fruta-e-fruto", "Fruta e Fruto", addTechnologies("html5") + addTechnologies("css3") + addTechnologies("bootstrap"),"Landing page de uma página de receitas feito com bootstrap.", "fruta-e-fruto-preview") + 
+
+    addProject("nlw-10-copa", "NLW Copa", addTechnologies("html5") + addTechnologies("css3") + addTechnologies("javascript"), "Projeto feito no evento da nlw copa promovido pela rocktseat. Este site mostra as melhores partidas que a fases de grupo da copa do mundo do Qatar 2022 poderá ter. ", "culturama-preview")    
---
+++ assets/js/star-project.js
@@ -0,0 +1,22 @@
+function addStarProject(linkName, iconName, name, iconTechnology) {
+    return `
+        <a href="https://github.com/Elasukou/${linkName}" target="_blank">
+            <div class="star-project">
+                <img src="assets/images/${iconName}.svg" class="image-star-project">
+                <div class="title-and-technology-project">
+                    <h3 class="title-star-project">${name}</h3>
+                    ${iconTechnology}
+                </div>
+            </div>
+        </a>
+    `
+}
+
+function addTechnologies(technology) {
+    return `
+        <img src="assets/images/${technology}.svg" class="iconTechnologyProject">
+    `
+}
+
+document.querySelector(".small-base-1").innerHTML = 
+    addStarProject("pricing-table", "prewie-pricing-table", "Desafio da tabela de preços rocketseat.", addTechnologies("html5") + addTechnologies("css3") + addTechnologies("bootstrap"))
