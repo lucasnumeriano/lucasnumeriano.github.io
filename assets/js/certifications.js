eb8950b94406261e42c1fee7706a7210d341847a
Lucas Numeriano <116529579+Elasukou@users.noreply.github.com>
2023-12-03 18:02:06 -0300

    update-13.1: styling certificate cards

--- assets/js/certifications.js
+++ assets/js/certifications.js
@@ -1,31 +1,28 @@
-function addCourse(logoCourse, linkCourse, courseName, courseDescription, validationCode) {
+function creatCourse(linkCourse, school, logoCourse, courseName, courseDescription, validationCode) {
     return `
-        <div class="course">
-            <img src="assets/images/${logoCourse}.svg" class="image-course">
-            <div class="title-and-description-course">
-                <a href="https://${linkCourse}" target="_blank" class="title-course">${courseName}</a>
-                <p class="description-course">${courseDescription}</p>
-                <p class="validation-course">${validationCode}</p>
+        <a href="https://${linkCourse}" target="_blank">
+            <div class="card">
+                <h5 class="school">${school}</h5>
+                <div class="course">
+                    <img src="assets/images/${logoCourse}.svg" class="image-course">
+                    <h5 class="title-course">${courseName}</h5>
+                    <p class="description-course">${courseDescription}</p>
+                    <p class="validation-course">${validationCode}</p>
+                </div>
             </div>
-        </div>
+        </a>
     `
 }
 
-document.querySelector("#certifications").innerHTML +=
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
+document.querySelector(".course-area").innerHTML =
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
  
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
 
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
 
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
 
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
 
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") + 
-
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
-
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4") +
-
-    addCourse("aluralogo", "cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "JavaScript e HTML", "desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4")
+    creatCourse("cursos.alura.com.br/certificate/afdeac5c-095b-46e9-a747-46ba5a593ef4", "Alura", "aluralogo", "JavaScript e HTML", "Desenvolva um jogo e pratique lógica de programação", "afdeac5c-095b-46e9-a747-46ba5a593ef4")
