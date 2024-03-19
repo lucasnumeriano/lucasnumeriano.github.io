53526b440b90de03157f8b6ac8864204966bcfac
Lucas Numeriano <116529579+Elasukou@users.noreply.github.com>
2024-03-18 23:21:07 -0300

    update-20
    Correct questions of SEO and Acessibility.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
-    main: ['./src/backtop.js', './src/certifications.js', './src/menu.js', './src/secondary-projects.js', './src/star-project.js']
+    main: ['./src/backtop.js', './src/certifications.js', './src/menu.js', './src/secondary-projects.js', './src/star-project.js', './src/index.js']
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
+  }
 };
