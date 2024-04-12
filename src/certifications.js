ec5ec28b4201cb1f60f071f5a5442550435c3694
Lucas Numeriano <lcsnumeriano@proton.me>
2024-04-12 15:31:02 -0300

    update-23.4
    Finaly ajust for publicate

--- src/certifications.js
+++ src/certifications.js
@@ -1,6 +1,6 @@
-const buttonSeeAll = document.querySelector('.button-see-all');
-const buttonSeeLess = document.querySelector('.button-see-less');
-const listContainers = document.querySelectorAll('.container');
+const buttonSeeAll = document.querySelector('.button-see-all')
+const buttonSeeLess = document.querySelector('.button-see-less')
+const listContainers = document.querySelectorAll('.container')
 
 const seeAll = document.querySelector('.see-all')
 const seeLess = document.querySelector('.see-less')
@@ -9,15 +9,15 @@ buttonSeeAll.addEventListener('click', () => {
     listContainers.forEach(container => {
         if (container.classList.contains('visible') && container.classList.contains('cont')) {
             container.classList.add('invisible')
-            container.classList.remove('visible');
-            seeAll.classList.remove('invisible');
-            seeLess.classList.remove('visible');
+            container.classList.remove('visible')
+            seeAll.classList.remove('invisible')
+            seeLess.classList.remove('visible')
             
         } else {
-            container.classList.remove('invisible');
-            container.classList.add('visible');
-            seeAll.classList.add('invisible');
-            seeLess.classList.add('visible');
+            container.classList.remove('invisible')
+            container.classList.add('visible')
+            seeAll.classList.add('invisible')
+            seeLess.classList.add('visible')
         }
-    });
-});
+    })
+})
